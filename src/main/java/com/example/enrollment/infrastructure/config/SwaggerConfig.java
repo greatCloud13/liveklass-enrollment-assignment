8b6d0682381f43ff200e86a19fd195779e66f59e
greatCloud13 <138741201+greatCloud13@users.noreply.github.com>
2026-04-23 21:13:02 +0900

    feat: SwaggerConfig 설정

---
+++ src/main/java/com/example/enrollment/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.example.enrollment.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("수강 신청 시스템 API")
+                        .description("강의 등록 및 수강 신청 관리 API")
+                        .version("v1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("X-User-Id"))
+                .components(new Components()
+                        .addSecuritySchemes("X-User-Id", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("X-User-Id")));
+    }
+
+}
+
